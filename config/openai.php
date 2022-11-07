f4c7f1370ec293686b94bf2600594bc033cf1efa
Domijuteur <115352483+Domijuteur@users.noreply.github.com>
2022-11-07 22:51:36 +0000

    sync submodule

--- config/openai.php
+++ config/openai.php
@@ -5,3 +5,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | OpenAI API Key and Organization
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify your OpenAI API Key and organization. This will be
