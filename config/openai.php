9fe66b2e909be68c242ba48ad1e084f84a7bbfdc
Domijuteur <115352483+Domijuteur@users.noreply.github.com>
2022-11-09 21:38:21 +0000

    Release v0.2.4

--- config/openai.php
+++ config/openai.php
@@ -9,3 +9,8 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may specify your OpenAI API Key and organization. This will be
+    | used to authenticate with the OpenAI API - you can find your API key
+    | and organization on your OpenAI dashboard, at https://openai.com.
+    */
+
+    'api_key' => env('OPENAI_API_KEY'),
