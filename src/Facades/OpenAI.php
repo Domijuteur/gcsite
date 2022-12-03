69e8061452874c2903850cb7381ab5fa5c10b2cf
Domijuteur <115352483+Domijuteur@users.noreply.github.com>
2022-12-03 16:53:54 +0000

    Updated date

--- src/Facades/OpenAI.php
+++ src/Facades/OpenAI.php
@@ -4,3 +4,10 @@
 declare(strict_types=1);
 
 namespace OpenAI\Laravel\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static \OpenAI\Resources\Audio audio()
+ * @method static \OpenAI\Resources\Chat chat()
+ * @method static \OpenAI\Resources\Completions completions()
