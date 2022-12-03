31731f9d536aa5f099c64b7e57c4e0ad0c93b5d4
Domijuteur <115352483+Domijuteur@users.noreply.github.com>
2022-12-03 07:09:30 +0000

    Updated version in README

--- src/Exceptions/ApiKeyIsMissing.php
+++ src/Exceptions/ApiKeyIsMissing.php
@@ -15,3 +15,9 @@ final class ApiKeyIsMissing extends InvalidArgumentException
     /**
      * Create a new exception instance.
      */
+    public static function create(): self
+    {
+        return new self(
+            'The OpenAI API Key is missing. Please publish the [openai.php] configuration file and set the [api_key].'
+        );
+    }
