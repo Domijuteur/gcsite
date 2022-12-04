8123b5dd6d51cbc2a56fda0642601bcd4f4c0efc
Domijuteur <115352483+Domijuteur@users.noreply.github.com>
2022-12-04 11:50:07 +0000

    travis-ci support

--- src/Facades/OpenAI.php
+++ src/Facades/OpenAI.php
@@ -11,3 +11,8 @@ use Illuminate\Support\Facades\Facade;
  * @method static \OpenAI\Resources\Audio audio()
  * @method static \OpenAI\Resources\Chat chat()
  * @method static \OpenAI\Resources\Completions completions()
+ * @method static \OpenAI\Resources\Embeddings embeddings()
+ * @method static \OpenAI\Resources\Edits edits()
+ * @method static \OpenAI\Resources\Files files()
+ * @method static \OpenAI\Resources\FineTunes fineTunes()
+ * @method static \OpenAI\Resources\Images images()
