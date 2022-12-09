b5ba0ca0262be0e25f62c6538f0d44a0e0add613
Domijuteur <115352483+Domijuteur@users.noreply.github.com>
2022-12-09 20:58:51 +0000

    Use new infra_spec_helper for gem dependencies

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,3 +4,8 @@
 declare(strict_types=1);
 
 namespace OpenAI\Laravel;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use OpenAI;
+use OpenAI\Client;
+use OpenAI\Laravel\Exceptions\ApiKeyIsMissing;
