0342f74f5fa6bb1a30c5b57a758955c592beedb9
Domijuteur <115352483+Domijuteur@users.noreply.github.com>
2022-12-13 07:10:18 +0000

    Bump lodash from 4.17.4 to 4.17.15

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -9,3 +9,8 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use OpenAI;
 use OpenAI\Client;
 use OpenAI\Laravel\Exceptions\ApiKeyIsMissing;
+
+/**
+ * @internal
+ */
+final class ServiceProvider extends BaseServiceProvider
