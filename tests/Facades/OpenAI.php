e82c0e62697c51303928869168b7544176a181d7
Domijuteur <115352483+Domijuteur@users.noreply.github.com>
2023-02-02 18:37:48 +0000

    updated the todo list

--- tests/Facades/OpenAI.php
+++ tests/Facades/OpenAI.php
@@ -1,2 +1,9 @@
 
 <?php
+
+use Illuminate\Config\Repository;
+use OpenAI\Laravel\Facades\OpenAI;
+use OpenAI\Laravel\ServiceProvider;
+use OpenAI\Resources\Completions;
+
+it('resolves resources', function () {
