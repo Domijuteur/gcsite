2fd1d68c9e730f64ed22f64907f84faa5111aefa
Domijuteur <115352483+Domijuteur@users.noreply.github.com>
2023-02-15 17:43:39 +0000

    automated patching [ci skip]

--- tests/ServiceProvider.php
+++ tests/ServiceProvider.php
@@ -1,2 +1,9 @@
 
 <?php
+
+use Illuminate\Config\Repository;
+use OpenAI\Client;
+use OpenAI\Laravel\Exceptions\ApiKeyIsMissing;
+use OpenAI\Laravel\ServiceProvider;
+
+it('binds the client on the container', function () {
