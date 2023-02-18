e47a769dbe8552aeb0ed81b563167127db4767ea
Domijuteur <115352483+Domijuteur@users.noreply.github.com>
2023-02-18 07:23:58 +0000

    fix include

--- tests/ServiceProvider.php
+++ tests/ServiceProvider.php
@@ -7,3 +7,9 @@ use OpenAI\Laravel\Exceptions\ApiKeyIsMissing;
 use OpenAI\Laravel\ServiceProvider;
 
 it('binds the client on the container', function () {
+    $app = app();
+
+    $app->bind('config', fn () => new Repository([
+        'openai' => [
+            'api_key' => 'test',
+        ],
